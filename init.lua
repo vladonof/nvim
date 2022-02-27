6d1a5ea57a6ec98691ca08274be8f9dcb54738f5
Vlad Onofrei <vlad.onofrei96@gmail.com>
2022-02-27 22:22:19 +0000

    add lsp config, remove coc

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ require "paq" {
     'nvim-treesitter/nvim-treesitter';
     'kyazdani42/nvim-web-devicons';
     {"lervag/vimtex", opt=true};     
-    {'neoclide/coc.nvim', branch = 'release'};
     'folke/tokyonight.nvim';
     'nvim-lualine/lualine.nvim';
     'nvim-telescope/telescope.nvim';
@@ -15,6 +14,8 @@ require "paq" {
     'kyazdani42/nvim-tree.lua';
     'windwp/nvim-ts-autotag';
     'beauwilliams/focus.nvim';
+    'jose-elias-alvarez/nvim-lsp-ts-utils';
+    {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' };
 }
 
 require('basics')
@@ -22,3 +23,4 @@ require('nvim-tree-custom')
 require('maps')
 require('telescope-config')
 require('lualine').setup()
+require('lsp-config')
